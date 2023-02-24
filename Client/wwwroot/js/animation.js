e23e4f1273252476e051889ae87f8d493bd90867
SlashRobotics <akl003@protonmail.com>
2023-02-24 15:12:01 -0800

    Animations Integration

---
+++ Client/wwwroot/js/animation.js
@@ -0,0 +1,8 @@
+﻿/*Resources Used & References:
+  https://michalsnik.github.io/aos/
+  https://github.com/michalsnik/aos
+*/
+
+document.addEventListener("load", function () {
+    AOS.init();
+});
