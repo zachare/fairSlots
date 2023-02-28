f3bb591c9776bab53f237410813cbe115cd5caaf
SlashRobotics <akl003@protonmail.com>
2023-02-27 19:50:52 -0800

    Added animations (Animation.razor, Animations.cs, animation.js)

---
+++ Client/wwwroot/Data/Animations.cs
@@ -0,0 +1,40 @@
+﻿namespace fairSlots.Server.Data
+{
+    /*Resources Used & References: 
+      https://michalsnik.github.io/aos/
+      https://github.com/michalsnik/aos
+    */
+    public static class Animations
+    {
+        // Fade animations
+        public const string fade = "fade";
+        public const string fadeUp = "fade-up";
+        public const string fadeDown = "fade-down";
+        public const string fadeLeft = "fade-left";
+        public const string fadeRight = "fade-right";
+
+        // Slide animations
+        public const string slideUp = "slide-up";
+        public const string slideDown = "slide-down";
+        public const string slideLeft = "slide-left";
+        public const string slideRight = "slide-right";
+
+        // Flip animations
+        public const string flipUp = "flip-up";
+        public const string flipDown = "flip-down";
+        public const string flipLeft = "flip-left";
+        public const string flipRight = "flip-right";
+
+        // Zoom animations
+        public const string zoomIn = "zoom-in";
+        public const string zoomInUp = "zoom-in-up";
+        public const string zoomInDown = "zoom-in-down";
+        public const string zoomInLeft = "zoom-in-left";
+        public const string zoomInRight = "zoom-in-right";
+        public const string zoomOut = "zoom-out";
+        public const string zoomOutUp = "zoom-out-up";
+        public const string zoomOutDown = "zoom-out-down";
+        public const string zoomOutLeft = "zoom-out-left";
+        public const string zoomOutRight = "zoom-out-right";
+    }
+}
---
+++ Client/wwwroot/js/animation.js
@@ -0,0 +1,8 @@
+﻿/*Resources Used & References:
+  https://michalsnik.github.io/aos/
+  https://github.com/michalsnik/aos
+*/
+
+window.addEventListener("load", function () {
+    AOS.init();
+});
